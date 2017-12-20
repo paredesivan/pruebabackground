07617dd987ebeb347aaa2def881c247bc1de8c51
Ivan Paredes <ip@airtrack.com.ar>
2017-12-20 15:48:52 +0000

    con plugin background

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,15 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.red_folder.phonegap.plugin.backgroundservice.BackgroundService",
+        "file": "plugins/com.red_folder.phonegap.plugin.backgroundservice/www/backgroundService.js",
+        "pluginId": "com.red_folder.phonegap.plugin.backgroundservice"
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.red_folder.phonegap.plugin.backgroundservice": "2.0.0"
+};
+// BOTTOM OF METADATA
+});
